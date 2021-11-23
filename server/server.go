6d4b691983e48a8a6300f5921a46b967c88ee6b9
Alexander Schiller-Stefansson <alesc@itu.dk>
2021-11-23 21:18:28 +0100

    MultipleServers With Linked Clients can now be created. Up to 20

--- server/server.go
+++ server/server.go
@@ -73,14 +73,18 @@ func (s *Server) JoinCommunication(ctx context.Context, request *replicatepb.Joi
 	log.SetOutput(&s.ServerLogFile)
 	s.LamportTime.update(int(request.LamportTime))
 
-	fmt.Println("Got a call")
-	
-
 	s.HighestServerID++
 	s.AllServerIDs[s.HighestServerID] = true
 	s.LamportTime.increment()
 
-	//Create new CLient and informOther
+	fmt.Printf("Server with ID: %s - Time: %s - Joined the ServerCommunication.\n", strconv.Itoa(int(s.HighestServerID)), strconv.Itoa(s.LamportTime.time))
+	log.Printf("Server with ID: %s - Time: %s - Joined the ServerCommunication.\n", strconv.Itoa(int(s.HighestServerID)), strconv.Itoa(s.LamportTime.time))
+
+	for i := 1; i < int(s.HighestServerID); i++ {
+		if s.AllServerIDs[i] {
+			s.serverInformationUpdate(s.Clients[i], replicatepb.SIUpdateRequest{LamportTime: int32(s.LamportTime.time), ID: s.ServerID, ChangeName: "ServerJoined", NewValue: []int32{s.HighestServerID}})
+		}	
+	}
 
 	return &replicatepb.JoinResponse{LamportTime: int32(s.LamportTime.time), ID: s.ServerID, HighestServerID: s.HighestServerID, AllServerIDs: s.AllServerIDs}, nil
 }
@@ -88,7 +92,7 @@ func (s *Server) JoinCommunication(ctx context.Context, request *replicatepb.Joi
 func (s *Server) ServerInformationUpdate(ctx context.Context, request *replicatepb.SIUpdateRequest) (*replicatepb.SIUpdateResponse, error) {
 	log.SetOutput(&s.ServerLogFile)
 	s.LamportTime.update(int(request.LamportTime))
-	
+
 	switch request.ChangeName {
 		case "ChangeYourServerID":
 			s.ServerID = request.NewValue[0]
@@ -104,8 +108,13 @@ func (s *Server) ServerInformationUpdate(ctx context.Context, request *replicate
 					s.AllServerIDs[i] = false
 				}				
 			}
+		case "CreateNewClient":
+			s.createNewServerClient(int(request.ID))
+			fmt.Println("Done")
 	}
 	
+	fmt.Printf("Server with Id: %s - Time: %s - Parsed the %s change.\n", strconv.Itoa(int(s.ServerID)), strconv.Itoa(s.LamportTime.time), request.ChangeName)
+	log.Printf("Server with Id: %s - Time: %s - Parsed the %s change.\n", strconv.Itoa(int(s.ServerID)), strconv.Itoa(s.LamportTime.time), request.ChangeName)
 	s.LamportTime.increment()
 	return &replicatepb.SIUpdateResponse{LamportTime: int32(s.LamportTime.time), ID: s.ServerID}, nil
 }
@@ -126,6 +135,9 @@ func (s *Server) IsAlive(ctx context.Context, request *replicatepb.Poke) (*repli
 	s.LamportTime.update(int(request.LamportTime))
 	s.LamportTime.increment()
 
+	fmt.Printf("Server with Id: %s - Time: %s - Throws Hand Sign and is still alive.\n", strconv.Itoa(int(s.ServerID)), strconv.Itoa(s.LamportTime.time))
+	log.Printf("Server with Id: %s - Time: %s - Throws Hand Sign and is still alive.\n", strconv.Itoa(int(s.ServerID)), strconv.Itoa(s.LamportTime.time))
+
 	return &replicatepb.HandSign{LamportTime: int32(s.LamportTime.time), ID: s.ServerID}, nil
 }
 //
@@ -135,6 +147,9 @@ func (s *Server) IsAlive(ctx context.Context, request *replicatepb.Poke) (*repli
 func (s *Server) joinCommunication(client replicatepb.ServerCommunicationClient, request replicatepb.JoinRequest) {
 	s.LamportTime.increment()
 	request.LamportTime = int32(s.LamportTime.time)
+
+	fmt.Printf("New Server - Sending Joining-Request.\n")
+	log.Printf("New Server - Sending Joining-Request.\n")
 	
 	response, err := client.JoinCommunication(context.Background(), &request)
 	if err != nil {
@@ -144,11 +159,15 @@ func (s *Server) joinCommunication(client replicatepb.ServerCommunicationClient,
 	s.HighestServerID = response.HighestServerID
 	s.AllServerIDs = response.AllServerIDs
 	s.LamportTime.update(int(response.LamportTime))
-	fmt.Println("I Parsed")
+	fmt.Printf("Server with Id: %s - Time: %s - Initial values set based on JoinResponse.\n", strconv.Itoa(int(s.ServerID)), strconv.Itoa(s.LamportTime.time))
+	log.Printf("Server with Id: %s - Time: %s - Initial values set based on JoinResponse.\n", strconv.Itoa(int(s.ServerID)), strconv.Itoa(s.LamportTime.time))
 }
 func (s *Server) serverInformationUpdate(client replicatepb.ServerCommunicationClient, request replicatepb.SIUpdateRequest) {
 	s.LamportTime.increment()
 	request.LamportTime = int32(s.LamportTime.time)
+
+	fmt.Printf("Server with Id: %s - Time: %s - Sends update-request with the change: %s.\n", strconv.Itoa(int(s.ServerID)), strconv.Itoa(s.LamportTime.time), request.ChangeName)
+	log.Printf("Server with Id: %s - Time: %s - Sends update-request with the change: %s.\n", strconv.Itoa(int(s.ServerID)), strconv.Itoa(s.LamportTime.time), request.ChangeName)
 
 	response, err := client.ServerInformationUpdate(context.Background(), &request)
 	if err != nil {
@@ -169,6 +188,10 @@ func (s *Server) auctionInformationUpdate(client replicatepb.ServerCommunication
 func (s *Server) isAlive(client replicatepb.ServerCommunicationClient, request replicatepb.Poke) {
 	s.LamportTime.increment()
 	request.LamportTime = int32(s.LamportTime.time)
+
+	fmt.Printf("Server with Id: %s - Time: %s - Pokes to check, if target is alive.\n", strconv.Itoa(int(s.ServerID)), strconv.Itoa(s.LamportTime.time))
+	log.Printf("Server with Id: %s - Time: %s - Pokes to check, if target is alive.\n", strconv.Itoa(int(s.ServerID)), strconv.Itoa(s.LamportTime.time))
+
 
 	response, err := client.IsAlive(context.Background(), &request)
 	if err != nil {
@@ -200,6 +223,24 @@ func (s *Server) changeHostPort() {
 	grpcServer.Serve(listener)
 
 }
+
+func (s *Server) createNewServerClient(TargetServerID int){
+	fmt.Printf("1\n")
+	var hostString = ":" + strconv.Itoa(8080+TargetServerID)
+	var key = "server" + strconv.Itoa(TargetServerID)
+	var tcpServer = flag.String(key, hostString , "TCP server")
+	var callerOpts []grpc.DialOption
+	callerOpts = append(callerOpts, grpc.WithBlock(), grpc.WithInsecure())
+	fmt.Printf("2 - %s - %s\n", key,hostString)
+	conn, err := grpc.Dial(*tcpServer, callerOpts...)
+	if err != nil {
+		log.Fatalf("Invalid Target Server. Not posible to dial.\n")
+	}
+	fmt.Printf("3\n")
+	client := replicatepb.NewServerCommunicationClient(conn)
+	s.Clients[TargetServerID] = client
+	fmt.Printf("New client created\n")
+}
 //
 //
 //
@@ -213,6 +254,10 @@ func newServer() *Server {
 		Clients: make([]replicatepb.ServerCommunicationClient, 20),
 	}
 	return s
+}
+func newClient(s Server) string{
+	s.serverInformationUpdate(s.Clients[0], replicatepb.SIUpdateRequest{LamportTime: int32(s.LamportTime.time), ID: s.ServerID, ChangeName: "CreateNewClient"})
+	return "true"
 }
 
 func main(){
@@ -239,8 +284,8 @@ func main(){
 	callerOpts = append(callerOpts, grpc.WithBlock(), grpc.WithTimeout(time.Duration(2)*time.Second), grpc.WithInsecure())
 	conn, err := grpc.Dial(*tcpServer, callerOpts...)
 	if err != nil {
-		log.Printf("Fail to dial leader when joinging, setting myself to leader\n")
-		fmt.Printf("Fail to dial leader when joinging, setting myself to leader\n")
+		log.Printf("Fail to dial leader when joining, setting myself to leader\n")
+		fmt.Printf("Fail to dial leader when joining, setting myself to leader\n")
 		s.ServerID = 0
 		s.HighestServerID = 0
 		s.AllServerIDs[0] = true
@@ -248,6 +293,10 @@ func main(){
 		client := replicatepb.NewServerCommunicationClient(conn)
 		s.Clients[0] = client
 		s.joinCommunication(s.Clients[0], replicatepb.JoinRequest{LamportTime: int32(s.LamportTime.time), ID: s.ServerID})
+		parallel := make(chan string)
+		go func() {
+			parallel <- newClient(*s)
+		}()
 	}
 
 	//Setting up Listener to the correct port in regards to ServerID
@@ -261,6 +310,7 @@ func main(){
 	grpcS := grpc.NewServer(opts...)
 	grpcServer := grpcS
 	replicatepb.RegisterServerCommunicationServer(grpcServer, s)
-	fmt.Println("Server up and lisining")
+	fmt.Printf("Server with ID: %s - Is up and lisining.\n", strconv.Itoa(int(s.ServerID)))
+	log.Printf("Server with ID: %s  - Is up and lisining.\n", strconv.Itoa(int(s.ServerID)))
 	grpcServer.Serve(listener)
 }
